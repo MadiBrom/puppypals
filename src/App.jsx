4d821c8c7f5d7d5ace904a4386668557003b78d7
Madi Bromfield <madibromfield215@gmail.com>
2024-08-19 23:23:40 -0500

    pretty stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
 import "./App.css";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Email: {featuredPup.email}</li>
+            <li>Age: {featuredPup.age}</li>
+            <li>
+              Tricks:
+              {featuredPup.tricks.length > 0 ? (
+                <ul>
+                  {featuredPup.tricks.map((trick) => (
+                    <li key={trick.id}>{trick.title}</li>
+                  ))}
+                </ul>
+              ) : (
+                " No tricks"
+              )}
+            </li>
+            <li>Is Cute: {featuredPup.isCute ? "Yes" : "No"}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
